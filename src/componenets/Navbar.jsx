9418e8aa5356d0436540a213ede73663cb357df2
unknown <arifhassansky@gmail.com>
2025-03-09 02:52:51 +0600

    added login and register page

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,9 +1,12 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaShoppingBag, FaSearch } from "react-icons/fa";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="sticky top-0 pt-4 bg-white/90">
       <div className="flex items-center justify-between w-10/12 mx-auto">
@@ -18,26 +21,81 @@ const Navbar = () => {
         </div>
 
         {/* Navigation Links */}
-        <div className="flex items-center gap-6 text-gray-900">
-          <Link href="/" className="hover:text-red-500">
+        <div className="flex items-center gap-6">
+          <Link
+            href="/"
+            className={`hover:text-red-500 ${
+              pathname === "/"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
             Home
           </Link>
-          <Link href="/about" className="hover:text-red-500">
+          <Link
+            href="/about"
+            className={`hover:text-red-500 ${
+              pathname === "/about"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
             About
           </Link>
-          <Link href="/services" className="hover:text-red-500">
+          <Link
+            href="/services"
+            className={`hover:text-red-500 ${
+              pathname === "/services"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
             Services
           </Link>
-          <Link href="/blog" className="hover:text-red-500">
+          <Link
+            href="/blog"
+            className={`hover:text-red-500 ${
+              pathname === "/blog"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
             Blog
           </Link>
-          <Link href="/contact" className="hover:text-red-500">
+          <Link
+            href="/contact"
+            className={`hover:text-red-500 ${
+              pathname === "/contact"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
             Contact
           </Link>
         </div>
 
-        {/* Icons and Button */}
+        {/* Icons and Appointment Button */}
         <div className="flex items-center gap-4">
+          <Link
+            href="/login"
+            className={`hover:text-red-500 ${
+              pathname === "/login"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
+            Login
+          </Link>
+          <Link
+            href="/signUp"
+            className={`hover:text-red-500 ${
+              pathname === "/signUp"
+                ? "bg-red-500 text-white px-4 py-1 rounded-md hover:text-white"
+                : "text-gray-900"
+            }`}
+          >
+            SignUp
+          </Link>
           <FaShoppingBag className="text-gray-700 cursor-pointer hover:text-red-500" />
           <FaSearch className="text-gray-700 cursor-pointer hover:text-red-500" />
           <Link
